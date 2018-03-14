07f035f674b16069b3084274cf5eccca6dc31f9e
Andrey Evstifeev <andrey.evstifeev@gmail.com>
2018-03-14 20:03:37 +0300

    Scala2 week3 assignment wip

---
+++ basics2/week3/assignment1/src/main/scala/quickcheck/QuickCheck.scala
@@ -0,0 +1,41 @@
+package quickcheck
+
+import common._
+
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop._
+
+abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
+
+
+  lazy val genHeap: Gen[H] = oneOf(
+    const(empty),
+    for {
+      k <- arbitrary[Int]
+      m <- oneOf(const(empty), genHeap)
+    } yield insert(k, m)
+  )
+
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  // If you insert any two elements into an empty heap,
+  // finding the minimum of the resulting heap should get the smallest of the two elements back.
+
+  property("findMinOfTwo") = forAll { (a: Int, b: Int) =>
+    val h = insert(b, insert(a, empty))
+
+    findMin(h) == Math.min(a, b)
+  }
+}
