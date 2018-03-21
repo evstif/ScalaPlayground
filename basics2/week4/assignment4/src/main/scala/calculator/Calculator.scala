64a7c803dadb378ba19a178d3feaf2babfdf8f28
evstif <andrey.evstifeev@gmail.com>
2018-03-22 00:00:52 +0300

    Scala2 week4 assignment wip - 3 of 3

--- basics2/week4/assignment4/src/main/scala/calculator/Calculator.scala
+++ basics2/week4/assignment4/src/main/scala/calculator/Calculator.scala
@@ -9,24 +9,37 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    namedExpressions.foldLeft(Map.empty[String, Signal[Double]])((map, tuple) => {
+      map.updated(tuple._1, new Signal(eval(tuple._2(), namedExpressions)))
+    })
+  }
+
+  def eval(expr: Expr, references: Map[String, Signal[Expr]], refsNames: Set[String]): Double = {
+    expr match {
+      case l:Literal => l.v
+      case p:Plus => eval(p.a, references) + eval(p.b, references)
+      case m:Minus => eval(m.a, references) - eval(m.b, references)
+      case t:Times => eval(t.a, references) * eval(t.b, references)
+      case d:Divide => eval(d.a, references) + eval(d.b, references)
+      case r:Ref => {
+        if (refsNames.contains(r.name))
+          Double.NaN
+        else
+          eval(getReferenceExpr(r.name, references), references, refsNames + r.name)
+      }
+      case _ => throw new IllegalArgumentException("expr")
+    }
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    eval(expr, references, Set.empty[String])
   }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
-  private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
-    references.get(name).fold[Expr] {
-      Literal(Double.NaN)
-    } { exprSignal =>
-      exprSignal()
-    }
+  private def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]) = {
+    references.get(name).fold[Expr](Literal(Double.NaN)) { exprSignal => exprSignal() }
   }
 }
--- basics2/week4/assignment4/src/main/scala/calculator/Polynomial.scala
+++ basics2/week4/assignment4/src/main/scala/calculator/Polynomial.scala
@@ -2,14 +2,12 @@
 package calculator
 
 object Polynomial {
-  
-  def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
+
+  def computeDelta(a: Signal[Double], b: Signal[Double], c: Signal[Double]): Signal[Double] = {
     new Signal(Math.pow(b(), 2) - 4*a()*c())
   }
 
-  def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+  def computeSolutions(a: Signal[Double], b: Signal[Double], c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
     val ops = List[(Double, Double) => Double](_ - _, _ + _)
     new Signal( if (delta() > 0.0) ops.map(op => op(-b(), Math.sqrt(delta())) / (2 * a())).toSet else Set() )
   }
